906b3a4f07d60a185d346d7414ea11f12128b7d7
madsejler <112628488+madsejler@users.noreply.github.com>
2022-12-14 12:46:36 +0100

    d

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 # Load packages (comments for more special stuff)
-
-import pandas as pd
-import pickle # un-pickling stuff from training notebook
-from xgboost import XGBRegressor # we use a trained XGBoost model...and therefore need to load it
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
-import shap # add prediction explainability
-
-import numpy as np
-import itertools # we need that to flatten ohe.categories_ into one list for columns
 import streamlit as st
-from streamlit_shap import st_shap # wrapper to display nice shap viz in the app
-import plotly.express as px
-import time
 
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
 ##Streamlit interface:
 st.set_page_config(page_title='Bank Marketing Project',
